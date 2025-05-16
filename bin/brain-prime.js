50566428ce425090274d0a3cc5f49da46ac4fb54
Leraa <akshinav@inbox.ru>
2025-05-16 11:07:55 +0300

    fin1

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,4 +3,4 @@
 import runEngine from '../src/index.js'
 import generateRound, { description } from '../src/games/prime.js'
 
-runEngine(description, generateRound)
+runEngine(description, generateRound);
